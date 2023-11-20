4e4ee2983e6cf871c3112f946126127654673a22
ctndiaye <ctndiaye26@gmail.com>
2023-11-20 10:33:48 -0500

    Revert "Add jenkin file"

--- educatrice/urls.py
+++ educatrice/urls.py
@@ -1,29 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import EducatriceViewSet, PresenceViewSet
+from .views import EducatriceViewSet
 
 urlpatterns = [
-    # Educatrice
     path('educatrices', EducatriceViewSet.as_view({
         'get': 'rechercher',
         'post': 'creer'
     })),
-    path('educatrices/<int:id>', EducatriceViewSet.as_view({
+    path('educatrices/<str:id>', EducatriceViewSet.as_view({
         'get': 'consulter',
         'put': 'modifier',
-        'post': 'modifier_mot_de_passe',
         'delete': 'supprimer'
-    })),
-    path('educatrices/connect', EducatriceViewSet.as_view({
-        'post': 'connect',
-    })),
-    # Presence
-    path('presences', PresenceViewSet.as_view({
-        'get': 'rechercher',
-        'post': 'creer'
-    })),
-    path('presences/<str:id>', PresenceViewSet.as_view({
-        'get': 'rechercher_par_educatrice',
     })),
 ]
