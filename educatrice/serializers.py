e2aaedc9ac65e8a261d61e8338072a69332aba2e
Cheick NDIAYE <ctndiaye26@gmail.com>
2024-06-06 12:16:26 -0400

    Misen place de test avec sqLite

--- educatrice/serializers.py
+++ educatrice/serializers.py
@@ -2,13 +2,14 @@ from rest_framework import serializers
 
 from .models import Educatrice, Presence
 
+
 class EducatriceSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Educatrice
-        fields='__all__'
+        model = Educatrice
+        fields = '__all__'
+
 
 class PresenceSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Presence
-        fields='__all__'
-    
+        model = Presence
+        fields = '__all__'
--- educatrice/tests/tests_educatrice.py
+++ educatrice/tests/tests_educatrice.py
@@ -1,17 +1,28 @@
 from django.test import TestCase
-from datetime import datetime
 import pytest
+import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "garderie_educatrice.settings")
+
+import django
+
+django.setup()
+
+from django.core.management import call_command
 from educatrice.models import Educatrice
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "garderie_educatrice.settings")
+
+django.setup()
 
 pytestmark = pytest.mark.django_db
 
+
 @pytest.mark.django_db
 class EducatriceTestClass(TestCase):
-
     databases = ['test_db', 'default']
 
-    create ="""{
+    create = """{
         "last_name": "SAMIRA",
         "first_name":"Samira",
         "adresse":"Montréal NORD",
@@ -27,14 +38,13 @@ class EducatriceTestClass(TestCase):
         "is_active": true
     }"""
 
-
     @classmethod
     def setUpTestData(cls):
-        Educatrice.objects.create(last_name="last_name", first_name="first_name", adresse="adresse", 
-                                  nom_contact="nom_contact", prenom_contact="prenom_contact", 
-                                  telephone_contact="telephone_contact", sexe="sexe", telephone="telephone", 
-                                  est_qualifie=True, username="user!r@", password="s@pwd!r1", 
-                                  email="email@gmail.com", is_active= True)
+        Educatrice.objects.create(last_name="last_name", first_name="first_name", adresse="adresse",
+                                  nom_contact="nom_contact", prenom_contact="prenom_contact",
+                                  telephone_contact="telephone_contact", sexe="sexe", telephone="telephone",
+                                  est_qualifie=True, username="user!r@", password="s@pwd!r1",
+                                  email="email@gmail.com", is_active=True)
         print("setUpTestData: Run once to set up non-modified data for all class methods.")
         pass
 
@@ -46,3 +56,14 @@ class EducatriceTestClass(TestCase):
     def test_false_is_false(self):
         print("Method: test_false_is_false.")
         self.assertFalse(False)
+
+    @pytest.mark.django_db
+    def test_create_educatice(self):
+        print("Method: test_create_educatice.")
+        educatrice = Educatrice \
+            .objects. \
+            create(last_name="Mejri", first_name="Anissa", adresse="adresse", nom_contact="NDIAYE",
+                   prenom_contact="Khalil", telephone_contact="1122334455", sexe="masculin",
+                   telephone="2211335544", est_qualifie=True, username="@n!ssA", password="mEjr!",
+                   email="anissa@gmail.com", is_active=True)
+        self.assertIsNotNone(educatrice, "Educatrice créé")
